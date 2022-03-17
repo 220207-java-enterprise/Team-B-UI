97f18a9641af191198b525b6fced3e20c09ffb7c
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-03-17 09:49:32 -0700

    Playing with React

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to React, Aji!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://localhost:4001/ers/users"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Users
         </a>
       </header>
     </div>
