8fce82232c8653af57d8cce37f9db41ce7e882a2
Aidan Amato <aidanamato@comcast.net>
2022-03-22 11:31:46 -0400

    header changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,28 +5,7 @@ const Header = (props: {cookies: AppCookies}) => {
   
   return (
     <header>
-      <nav className='navbar navbar-expand navbar-dark bg-dark justify-content-between'>
-        <a href="/" className='navbar-brand ml-2'>
-          Employee Reimbursement System
-        </a>
-        <div>
-          {!token && (
-          <>
-          <a href="/login" className='navbar-brand'>
-            Login
-          </a>
-          <a href="/signup" className='navbar-brand'>
-            Signup
-          </a>
-          </>
-          )}
-          {token && (
-          <p className='navbar-brand my-0'>
-            Logout
-          </p>
-          )}
-        </div>
-      </nav>
+      
     </header>
   )
 }
