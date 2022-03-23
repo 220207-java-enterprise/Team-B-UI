f6632a98b8f02e59b7ad54b5f4c319f9b2666a09
Ajitesh Vedula <Ajitesh@Ajiteshs-MBP.hsd1.ca.comcast.net>
2022-03-23 12:20:58 -0700

    Dropdown menu working

--- src/components/ReimbursementList.tsx
+++ src/components/ReimbursementList.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import React, { SyntheticEvent, useEffect, useState } from "react";
 import AppCookies from "../interfaces/AppCookies";
 import ReimbursementService from "../services/ReimbursementService";
 import Reimbursement from '../interfaces/Reimbursement';
+import { sortAndDeduplicateDiagnostics } from "typescript";
 
 export const ReimbursementList = (props: {cookies: AppCookies}) => {
   const cookies = props.cookies;
@@ -9,6 +10,7 @@ export const ReimbursementList = (props: {cookies: AppCookies}) => {
   const role = cookies.principal?.role;
 
   const [reimbursements, setReimbursements] = useState([]);
+  const [selectIndex, setSelectIndex] = useState(0);
 
   useEffect(() => {
     if (role === "FINANCE MANAGER") {
@@ -20,10 +22,12 @@ export const ReimbursementList = (props: {cookies: AppCookies}) => {
             console.log(response.data);
             setReimbursements(response.data);
           } 
+
           else
             console.log(response.status);
         })
         .catch(error => console.log(error));
+
     }
 
     if (role === "EMPLOYEE") {
@@ -40,14 +44,143 @@ export const ReimbursementList = (props: {cookies: AppCookies}) => {
         })
         .catch(error => console.log(error));
     }
-  }, [cookies])
+  }, [cookies]);
+
+  useEffect(() => {
+    console.log("You changed the index")
+
+    switch (selectIndex){
+      case 0:
+      case 1:
+        ReimbursementService.getReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+      
+      case 2:
+        ReimbursementService.getPendingReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+
+        case 3:
+        ReimbursementService.getApprovedReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+
+        case 4:
+        ReimbursementService.getDeniedReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+
+        case 5:
+        ReimbursementService.getLodgingReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+
+        case 6:
+        ReimbursementService.getTravelReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+
+        case 7:
+        ReimbursementService.getFoodReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+
+        case 8:
+        ReimbursementService.getOtherReimbursements(token)
+          .then((response) => {
+          console.log(response);
+
+          if (response.status === 200) {
+            console.log(response.data);
+            setReimbursements(response.data);
+          } 
+          else
+            console.log(response.status);
+        })
+        .catch(error => console.log(error));
+        break;
+    }
+
+  }, [selectIndex]);
+
   
   return (
     <main id="reimbursement-list" className="container-fluid">
       <h2 className="whiteCenter">List Reimbursements</h2>
       
       <div className="table-elements">
-        <select className="btn btn-secondary dropdown-toggle">
+        <select id = "dropdown_list" className="btn btn-secondary dropdown-toggle" onChange = {(e) => setSelectIndex(e.target.selectedIndex)}>
           <option selected value="Filter by:">Filter by:</option>
           <option value="All">All</option>
           <option value="Status: PENDING">Status: PENDING</option>
@@ -59,6 +192,7 @@ export const ReimbursementList = (props: {cookies: AppCookies}) => {
           <option value="Type: OTHER">Type: OTHER</option>
         </select>
       </div>
+
       
       <table className="table table-bordered table-striped m-auto">
         <thead>
