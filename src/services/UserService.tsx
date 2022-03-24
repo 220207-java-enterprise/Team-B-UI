f65d4a656882c18b6a5ec51aa8a346fd04079fc0
Aidan Amato <aidanamato@comcast.net>
2022-03-24 17:52:29 -0400

    user-list styling/functionality

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -3,6 +3,8 @@ import axios, { AxiosRequestConfig } from "axios";
 const USERS_REST_API_URL = "http://localhost:4001/ers/users";
 const ACTIVATE_REST_API_URL = "http://localhost:4001/ers/users/approve";
 const LOGIN_REST_API_URL = "http://localhost:4001/ers/users/login";
+const GET_INACTIVE_API_URL = "http://localhost:4001/ers/users/inactive";
+const GET_ALL_API_URL = "http://localhost:4001/ers/users/all";
 
 const UserService = {
   getUsers: (token: string) => {
@@ -13,6 +15,26 @@ const UserService = {
       validateStatus: () => true
     };
     return axios.get(USERS_REST_API_URL, config);
+  },
+
+  getInactive: (token: string) => {
+    const config = {
+      headers: {
+        Authorization: token
+      },
+      validateStatus: () => true
+    };
+    return axios.get(GET_INACTIVE_API_URL, config);
+  },
+
+  getAll: (token: string) => {
+    const config = {
+      headers: {
+        Authorization: token
+      },
+      validateStatus: () => true
+    };
+    return axios.get(GET_ALL_API_URL, config);
   },
 
   createUser: (user: {}) => {
